e18b4e193176f75af9576014dcd7c2068ef59a65
Yaroslav Muravsky <y@myarik.com>
2016-09-22 19:18:46 +0300

    Load config

--- sbackup/utils.py
+++ sbackup/utils.py
@@ -3,8 +3,19 @@ from __future__ import (absolute_import, division, print_function, unicode_liter
 
 from .constants import codeCodes
 
+import yaml
+
 
 def stringc(text, color):
     """String in color."""
 
     return u"\033[%sm%s\033[0m" % (codeCodes[color], text)
+
+
+def load_config(filename: str):
+    """
+    Load yml config
+    """
+    with open(filename, 'rt') as file_config:
+        config = yaml.safe_load(file_config)
+    return config
